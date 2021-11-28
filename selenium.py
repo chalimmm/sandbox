1f9f96cfd44ef5e00e431d87ad7c3ed44b084f2a
Chandra Halim Nuruddin <39824546+chalimmm@users.noreply.github.com>
2021-11-29 03:12:17 +0700

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
+import json
+import re
+import time
+import sys
 import os
 
 chrome_options = webdriver.ChromeOptions()
@@ -9,5 +14,116 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 # Now you can start using Selenium
-driver.get("https://academic.ui.ac.id")
-print(driver.page_source)
+driver.get("https://academic.ui.ac.id/")
+
+username = driver.find_element_by_name("u")
+username.send_keys("chandra.halim")
+
+password = driver.find_element_by_name("p")
+password.send_keys("chaind.18")
+
+login = driver.find_element_by_css_selector("input[value='Login']")
+login.click()
+
+time.sleep(3)
+
+driver.get("https://academic.ui.ac.id/main/Schedule/Index")
+
+page_source = driver.page_source
+
+driver.close()
+
+cuts = []
+        
+for m in re.finditer('<table class="box">', page_source):
+    cuts.append([m.start(), m.end()])
+
+iter = 0
+for m in re.finditer('</table>', page_source):
+    cuts[iter][1] = m.end()
+    iter += 1
+    if iter >= len(cuts):
+        break
+
+filtered_page = ""
+for i in range(len(cuts)):
+    filtered_page += page_source[cuts[i][0]:cuts[i][1]]
+
+regexDay = '([A-Za-z]+, [0-9]+.[0-9]+-[0-9]+.[0-9]+)'
+
+soup = BeautifulSoup(filtered_page, 'html.parser')
+
+courses = soup.find_all("tr")
+
+elements = ""
+   
+for course in courses:
+    selected = course.find("th")
+    if selected is not None:
+        if ";" in selected.text:
+            elements += selected.text
+            elements += "--------------"
+    selected = course.find_all("td", nowrap="")
+    if len(selected) > 0:
+        for element in selected:
+            elements += element.text + "\n"
+        elements += "--------------"
+
+with open("output.txt", "w") as file:
+    file.write(str(elements))
+
+listElements = elements.split("--------------")
+
+courses = {}
+
+temp = ""
+
+for element in listElements:
+    element = element.strip()
+    if ";" in element and "-" not in element[:11]:
+        temp = element[:11].strip()
+        temp = temp.strip('\n')
+        # print(temp)
+        name = element[13:-28].strip()
+        name = name.strip('\n')
+        courses.update({
+            temp : {
+                'Nama' : name,
+                'Kelas' : []
+            }
+        })
+    elif temp != "":
+        tempClass = element.split('\n')
+        if (len(tempClass) > 1 and ';' in tempClass[1]):
+            break
+        elif len(tempClass) > 6 :
+            courses[temp]['Kelas'].append({
+                'Nama' : tempClass[1],
+                'Jadwal' : re.findall(regexDay, tempClass[4]),
+                'Ruang' : tempClass[5].strip("-"), 
+                'Dosen' : tempClass[6].strip("- ").split("- ")
+            })
+        elif len(tempClass) > 5 :
+            courses[temp]['Kelas'].append({
+                'Nama' : tempClass[1],
+                'Jadwal' : re.findall(regexDay, tempClass[4]),
+                'Ruang' : tempClass[5].strip("-"), 
+                'Dosen' : ''
+            })
+        elif len(tempClass) > 4 :
+            courses[temp]['Kelas'].append({
+                'Nama' : tempClass[1],
+                'Jadwal' : re.findall(regexDay, tempClass[4]),
+                'Ruang' : '', 
+                'Dosen' : ''
+            })
+        elif len(tempClass) > 3 :
+            courses[temp]['Kelas'].append({
+                'Nama' : tempClass[1],
+                'Jadwal' : '',
+                'Ruang' : '', 
+                'Dosen' : ''
+            })
+
+with open("output.json", "w") as file: 
+    file.write(json.dumps(courses, indent=4))
