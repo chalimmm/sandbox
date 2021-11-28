a0d3cf529337bc5879153223cdc543e2634792e6
Chandra Halim Nuruddin <39824546+chalimmm@users.noreply.github.com>
2021-11-28 22:26:39 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ headless = true
 port = $PORT
 enableCORS = false
 " > ~/.streamlit/config.toml
+
+sudo apt install google-chrome-stable
+sudo apt-get install firefox
+
+seleniumbase install chromedriver
+seleniumbase install geckodriver
